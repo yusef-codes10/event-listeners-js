047e66362c1dfc8442d56897c1851deafbcba6e4
Youcef <youcefcopy@gmail.com>
2025-11-22 12:00:14 +0100

    settign grandparent, parent & child

---
+++ script.js
@@ -0,0 +1,8 @@
+const grandParent = document.querySelector(".grand-parent");
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+console.log(grandParent);
+console.log(parent);
+console.log(child);
+
+
