fccbcfc43ddfe545ada22aa5bd0fa7cd3be797c4
Youcef <youcefcopy@gmail.com>
2025-11-24 12:31:41 +0100

    HTML injection!

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
-const username = document.getElementById('username');
-const clickBtn = document.getElementById('clickBtn');
-const dialog = document.querySelector('#dialog');
-console.log(dialog);
+const addBtn = document.getElementById("addBtn");
+const userInput = document.getElementById("userInput");
+const output = document.getElementById("output");
 
+addBtn.addEventListener("click", () => {
+  const value = userInput.value;
 
-clickBtn.addEventListener('click', () => {
-    dialog.showModal();
+  // UNSAFE on purpose, for testing
+  output.innerHTML = value;
 });
-
-const cancelBtn = document.getElementById('cancelBtn');
-cancelBtn.addEventListener('click', closeDialog)
-
-const userChoiceBtn = document.getElementById('userChoiceBtn') ;
-userChoiceBtn.addEventListener('click', () => {
-    if (usernameRadio.checked) {
-        console.log('username mode');
-        const usernameInput = document.createElement('input');
-        const userInputContainer = document.querySelector('.user-input');
-        usernameInput.classList = 'input';
-        userInputContainer.append(usernameInput);
-    } else if (ageRadio.checked) {
-        console.log('age mode');
-    }
-    closeDialog();
-
-})
-
-// radio 
-const usernameRadio = document.getElementById('usernameRadio');
-const ageRadio = document.getElementById('ageRadio');
-
-
-function closeDialog() {
-    dialog.close();
-}
-
-function createUserInput() {
-    
-}
