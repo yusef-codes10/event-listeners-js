cfc5ab65af1c48cc396d939fb8c7c1c9520d95e5
Youcef <youcefcopy@gmail.com>
2025-11-24 12:11:41 +0100

    dialog and bugs fixed  | no flex display on the dialog

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const username = document.getElementById('username');
 const clickBtn = document.getElementById('clickBtn');
+const dialog = document.querySelector('#dialog');
+console.log(dialog);
 
 
-clickBtn.addEventListener('click', () => window.alert(`your name is ${username.value}`));
+clickBtn.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+const cancelBtn = document.getElementById('cancel');
+cancelBtn.addEventListener('click', 
+    () => {
+        dialog.close();
+    }
+)
