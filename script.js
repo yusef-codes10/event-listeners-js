3727daae9f64274cfa2e383ebda9ed631061abac
Youcef <youcefcopy@gmail.com>
2025-11-24 12:19:49 +0100

    submit & close btns

--- script.js
+++ script.js
@@ -8,9 +8,25 @@ clickBtn.addEventListener('click', () => {
     dialog.showModal();
 });
 
-const cancelBtn = document.getElementById('cancel');
-cancelBtn.addEventListener('click', 
-    () => {
-        dialog.close();
+const cancelBtn = document.getElementById('cancelBtn');
+cancelBtn.addEventListener('click', closeDialog)
+
+const userChoiceBtn = document.getElementById('userChoiceBtn') ;
+userChoiceBtn.addEventListener('click', () => {
+    if (usernameRadio.checked) {
+        console.log('username mode');
+    } else if (ageRadio.checked) {
+        console.log('age mode');
     }
-)
+    closeDialog();
+
+})
+
+// radio 
+const usernameRadio = document.getElementById('usernameRadio');
+const ageRadio = document.getElementById('ageRadio');
+
+
+function closeDialog() {
+    dialog.close();
+}
